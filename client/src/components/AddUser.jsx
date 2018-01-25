feed62279806fd9f37eac1a8d5750af607959cbd
sasiliyu <sasil.adetunji@gmail.com>
2018-01-25 19:55:42 +0100

    adds react forms

---
+++ client/src/components/AddUser.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const AddUser = (props) => {
+  return (
+    <form onSubmit={(event) => props.addUser(event)}>
+      <div className="form-group">
+        <input
+          name="username"
+          className="form-control input-lg"
+          type="text"
+          placeholder="Enter a username"
+          value={props.username}
+          onChange={props.handleChange}
+
+          required
+        />
+      </div>
+      <div className="form-group">
+        <input
+          name="email"
+          className="form-control input-lg"
+          type="email"
+          placeholder="Enter an email address"
+          value={props.email}
+          onChange={props.handleChange}
+
+          required
+        />
+      </div>
+      <input
+        type="submit"
+        className="btn btn-primary btn-lg btn-block"
+        value="Submit"
+      />
+    </form>
+  )
+};
+
+export default AddUser;
